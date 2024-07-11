9ef18d22d3538a50fcbac35882c220b953c07eed
Yat Ho <lagoho7@gmail.com>
2024-07-11 14:58:32 +0800

    refactor: add new TCP code behind abstract interface

---
+++ libtransmission/peer-transport-tcp.cc
@@ -0,0 +1,389 @@
+// This file Copyright © Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
+// License text can be found in the licenses/ folder.
+
+#include <string_view>
+
+#ifndef _WIN32
+#include <netinet/tcp.h> // TCP_CONGESTION
+#endif
+
+#include <event2/event.h>
+
+#include <fmt/core.h>
+
+#include "libtransmission/transmission.h"
+
+#include "libtransmission/error.h"
+#include "libtransmission/net.h"
+#include "libtransmission/peer-transport.h"
+#include "libtransmission/tr-assert.h"
+#include "libtransmission/log.h"
+#include "libtransmission/utils-ev.h"
+
+#define tr_logAddErrorTcp(tcp, msg) tr_logAddError(msg, (tcp)->display_name())
+#define tr_logAddWarnTcp(tcp, msg) tr_logAddWarn(msg, (tcp)->display_name())
+#define tr_logAddDebugTcp(tcp, msg) tr_logAddDebug(msg, (tcp)->display_name())
+#define tr_logAddTraceTcp(tcp, msg) tr_logAddTrace(msg, (tcp)->display_name())
+
+namespace
+{
+tr_socket_t create_socket(int domain, int type)
+{
+    auto const sockfd = socket(domain, type, 0);
+    if (sockfd == TR_BAD_SOCKET)
+    {
+        if (auto err = sockerrno; err != EAFNOSUPPORT)
+        {
+            tr_logAddWarn(fmt::format(
+                _("Couldn't create socket: {error} ({error_code})"),
+                fmt::arg("error", tr_net_strerror(err)),
+                fmt::arg("error_code", err)));
+        }
+
+        return TR_BAD_SOCKET;
+    }
+
+    if (evutil_make_socket_nonblocking(sockfd) == -1)
+    {
+        tr_net_close_socket(sockfd);
+        return TR_BAD_SOCKET;
+    }
+
+    if (static bool buf_logged = false; !buf_logged)
+    {
+        int i = 0;
+        socklen_t size = sizeof(i);
+
+        if (getsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, reinterpret_cast<char*>(&i), &size) != -1)
+        {
+            tr_logAddTrace(fmt::format("SO_SNDBUF size is {}", i));
+        }
+
+        i = 0;
+        size = sizeof(i);
+
+        if (getsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, reinterpret_cast<char*>(&i), &size) != -1)
+        {
+            tr_logAddTrace(fmt::format("SO_RCVBUF size is {}", i));
+        }
+
+        buf_logged = true;
+    }
+
+    return sockfd;
+}
+
+tr_socket_t open_peer_socket(tr_address bind_address, tr_socket_address const& socket_address, bool client_is_seed)
+{
+    auto const& [addr, port] = socket_address;
+
+    TR_ASSERT(socket_address.is_valid());
+    if (!socket_address.is_valid())
+    {
+        return TR_BAD_SOCKET;
+    }
+
+    auto const s = create_socket(tr_ip_protocol_to_af(addr.type), SOCK_STREAM);
+    if (s == TR_BAD_SOCKET)
+    {
+        return TR_BAD_SOCKET;
+    }
+
+    // seeds don't need a big read buffer, so make it smaller
+    if (client_is_seed)
+    {
+        int const n = 8192;
+
+        if (setsockopt(s, SOL_SOCKET, SO_RCVBUF, reinterpret_cast<char const*>(&n), sizeof(n)) == -1)
+        {
+            tr_logAddDebug(fmt::format("Unable to set SO_RCVBUF on socket {}: {}", s, tr_net_strerror(sockerrno)));
+        }
+    }
+
+    if (auto const [src_ss, src_sslen] = tr_socket_address::to_sockaddr(bind_address, {});
+        bind(s, reinterpret_cast<sockaddr const*>(&src_ss), src_sslen) == -1)
+    {
+        tr_logAddWarn(fmt::format(
+            _("Couldn't set source address {address} on {socket}: {error} ({error_code})"),
+            fmt::arg("address", bind_address.display_name()),
+            fmt::arg("socket", s),
+            fmt::arg("error", tr_net_strerror(sockerrno)),
+            fmt::arg("error_code", sockerrno)));
+        tr_net_close_socket(s);
+        return TR_BAD_SOCKET;
+    }
+
+    if (auto const [tgt_ss, tgt_sslen] = socket_address.to_sockaddr();
+        connect(s, reinterpret_cast<sockaddr const*>(&tgt_ss), tgt_sslen) == -1 &&
+#ifdef _WIN32
+        sockerrno != WSAEWOULDBLOCK &&
+#endif
+        sockerrno != EINPROGRESS)
+    {
+        if (auto const tmperrno = sockerrno;
+            (tmperrno != ECONNREFUSED && tmperrno != ENETUNREACH && tmperrno != EHOSTUNREACH) || addr.is_ipv4())
+        {
+            tr_logAddWarn(fmt::format(
+                _("Couldn't connect socket {socket} to {address}:{port}: {error} ({error_code})"),
+                fmt::arg("socket", s),
+                fmt::arg("address", addr.display_name()),
+                fmt::arg("port", port.host()),
+                fmt::arg("error", tr_net_strerror(tmperrno)),
+                fmt::arg("error_code", tmperrno)));
+        }
+
+        tr_net_close_socket(s);
+        return TR_BAD_SOCKET;
+    }
+
+    tr_logAddTrace(fmt::format("New OUTGOING connection {} ({})", s, socket_address.display_name()));
+
+    return s;
+}
+
+void set_tos(tr_socket_t s, [[maybe_unused]] int tos, tr_address_type type)
+{
+    if (s == TR_BAD_SOCKET)
+    {
+        return;
+    }
+
+    if (type == TR_AF_INET)
+    {
+#if defined(IP_TOS) && !defined(_WIN32)
+        if (setsockopt(s, IPPROTO_IP, IP_TOS, reinterpret_cast<char const*>(&tos), sizeof(tos)) == -1)
+        {
+            tr_logAddDebug(fmt::format("Can't set TOS '{}': {}", tos, tr_net_strerror(sockerrno)));
+        }
+#endif
+    }
+    else if (type == TR_AF_INET6)
+    {
+#if defined(IPV6_TCLASS) && !defined(_WIN32)
+        if (setsockopt(s, IPPROTO_IPV6, IPV6_TCLASS, reinterpret_cast<char const*>(&tos), sizeof(tos)) == -1)
+        {
+            tr_logAddDebug(fmt::format("Can't set IPv6 ToS '{}': {}", tos, tr_net_strerror(sockerrno)));
+        }
+#endif
+    }
+    else
+    {
+        /* program should never reach here! */
+        tr_logAddDebug("Something goes wrong while setting TOS/Traffic-Class");
+    }
+}
+
+void set_congestion_control([[maybe_unused]] tr_socket_t s, [[maybe_unused]] std::string const& algorithm)
+{
+#ifdef TCP_CONGESTION
+    if (setsockopt(s, IPPROTO_TCP, TCP_CONGESTION, algorithm.c_str(), std::size(algorithm) + 1) == -1)
+    {
+        tr_logAddDebug(fmt::format("Can't set congestion control algorithm '{}': {}", algorithm, tr_net_strerror(sockerrno)));
+    }
+#endif
+}
+
+class tr_peer_tcp_impl final : public tr_peer_tcp
+{
+public:
+    tr_peer_tcp_impl(tr_socket_address const& socket_address, bool client_is_seed, std::unique_ptr<Mediator> mediator)
+        : tr_peer_tcp{ socket_address }
+        , fd_{ open_peer_socket(mediator->bind_address(address().type), socket_address, client_is_seed) }
+        , mediator_{ std::move(mediator) }
+    {
+        if (!is_valid())
+        {
+            tr_logAddTraceTcp(this, "failed to create socket (tcp)");
+            return;
+        }
+
+        set_tos(fd_, mediator_->tos(), address().type);
+
+        if (auto const& alg = mediator_->peer_congestion_algorithm(); !std::empty(alg))
+        {
+            set_congestion_control(fd_, alg);
+        }
+
+        event_read_.reset(event_new(mediator_->event_base(), fd_, EV_READ, event_read_cb, this));
+        event_write_.reset(event_new(mediator_->event_base(), fd_, EV_WRITE, event_write_cb, this));
+
+        tr_logAddTraceTcp(this, fmt::format("socket (tcp) is {}", fd_));
+    }
+
+    ~tr_peer_tcp_impl() override
+    {
+        tr_net_close_socket(fd_);
+        fd_ = TR_BAD_SOCKET;
+    }
+
+    // ---
+
+    [[nodiscard]] bool is_valid() const noexcept override
+    {
+        return tr_peer_transport::is_valid() && fd_ != TR_BAD_SOCKET;
+    }
+
+    // ---
+
+    size_t recv(InBuf& buf, size_t n_bytes, tr_error* error = nullptr) override
+    {
+        if (n_bytes == 0U)
+        {
+            return {};
+        }
+
+        auto const [p_buf, buflen] = buf.reserve_space(n_bytes);
+        n_bytes = std::min(n_bytes, buflen);
+        auto const n_read = ::recv(fd_, reinterpret_cast<char*>(p_buf), n_bytes, 0);
+        auto const err = sockerrno;
+
+        if (n_read > 0)
+        {
+            buf.commit_space(n_read);
+            return static_cast<size_t>(n_read);
+        }
+
+        // When a stream socket peer has performed an orderly shutdown,
+        // the return value will be 0 (the traditional "end-of-file" return).
+        if (error != nullptr)
+        {
+            if (n_read == 0)
+            {
+                error->set_from_errno(ENOTCONN);
+            }
+            else
+            {
+                error->set(err, tr_net_strerror(err));
+            }
+        }
+
+        return {};
+    }
+
+    size_t send(OutBuf& buf, size_t n_bytes, tr_error* error = nullptr) override
+    {
+        n_bytes = std::min(n_bytes, buf.size());
+
+        if (n_bytes == 0U)
+        {
+            return {};
+        }
+
+        if (auto const n_sent = ::send(fd_, reinterpret_cast<char const*>(buf.data()), n_bytes, 0); n_sent >= 0U)
+        {
+            buf.drain(n_sent);
+            return static_cast<size_t>(n_sent);
+        }
+
+        if (error != nullptr)
+        {
+            auto const err = sockerrno;
+            error->set(err, tr_net_strerror(err));
+        }
+
+        return {};
+    }
+
+    // ---
+
+    void set_enabled(tr_direction dir, bool is_enabled) override
+    {
+        TR_ASSERT(tr_isDirection(dir));
+
+        short const event = dir == TR_UP ? EV_WRITE : EV_READ;
+
+        if (is_enabled)
+        {
+            event_enable(event);
+        }
+        else
+        {
+            event_disable(event);
+        }
+    }
+
+private:
+    void event_enable(short event)
+    {
+        TR_ASSERT(event_read_);
+        TR_ASSERT(event_write_);
+
+        if ((event & EV_READ) != 0 && (pending_events_ & EV_READ) == 0)
+        {
+            tr_logAddTraceTcp(this, "enabling ready-to-read polling");
+            event_add(event_read_.get(), nullptr);
+            pending_events_ |= EV_READ;
+        }
+
+        if ((event & EV_WRITE) != 0 && (pending_events_ & EV_WRITE) == 0)
+        {
+            tr_logAddTraceTcp(this, "enabling ready-to-write polling");
+            event_add(event_write_.get(), nullptr);
+            pending_events_ |= EV_WRITE;
+        }
+    }
+
+    void event_disable(short event)
+    {
+        TR_ASSERT(event_read_);
+        TR_ASSERT(event_write_);
+
+        if ((event & EV_READ) != 0 && (pending_events_ & EV_READ) != 0)
+        {
+            tr_logAddTraceTcp(this, "disabling ready-to-read polling");
+            event_del(event_read_.get());
+            pending_events_ &= ~EV_READ;
+        }
+
+        if ((event & EV_WRITE) != 0 && (pending_events_ & EV_WRITE) != 0)
+        {
+            tr_logAddTraceTcp(this, "disabling ready-to-write polling");
+            event_del(event_write_.get());
+            pending_events_ &= ~EV_WRITE;
+        }
+    }
+
+    static void event_read_cb([[maybe_unused]] evutil_socket_t fd, short /*event*/, void* vtcp)
+    {
+        auto* const tcp = static_cast<tr_peer_tcp_impl*>(vtcp);
+        tr_logAddTraceTcp(tcp, "libevent says this TCP peer socket is ready for reading");
+
+        TR_ASSERT(tcp->fd_ == fd);
+
+        tcp->pending_events_ &= ~EV_READ;
+
+        tcp->mediator_->notify_read();
+    }
+
+    static void event_write_cb([[maybe_unused]] evutil_socket_t fd, short /*event*/, void* vtcp)
+    {
+        auto* const tcp = static_cast<tr_peer_tcp_impl*>(vtcp);
+        tr_logAddTraceTcp(tcp, "libevent says this TCP peer socket is ready for writing");
+
+        TR_ASSERT(tcp->fd_ == fd);
+
+        tcp->pending_events_ &= ~EV_WRITE;
+
+        tcp->mediator_->notify_write();
+    }
+
+    short pending_events_ = {};
+
+    tr_socket_t fd_ = TR_BAD_SOCKET;
+
+    libtransmission::evhelpers::event_unique_ptr event_read_;
+    libtransmission::evhelpers::event_unique_ptr event_write_;
+
+    std::unique_ptr<Mediator> mediator_;
+};
+} // namespace
+
+std::unique_ptr<tr_peer_tcp> tr_peer_tcp::create(
+    tr_socket_address const& socket_address,
+    bool client_is_seed,
+    std::unique_ptr<Mediator> mediator)
+{
+    return std::make_unique<tr_peer_tcp_impl>(socket_address, client_is_seed, std::move(mediator));
+}
---
+++ libtransmission/peer-transport.h
@@ -0,0 +1,105 @@
+// This file Copyright © Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
+// License text can be found in the licenses/ folder.
+
+#pragma once
+
+#ifndef __TRANSMISSION__
+#error only libtransmission should #include this header.
+#endif
+
+#include <cstddef>
+#include <memory>
+#include <string>
+
+#include "libtransmission/transmission.h"
+
+#include "libtransmission/net.h"
+#include "libtransmission/tr-buffer.h"
+
+struct event_base;
+
+class tr_peer_transport
+{
+public:
+    using InBuf = libtransmission::BufferWriter<std::byte>;
+    using OutBuf = libtransmission::BufferReader<std::byte>;
+
+    struct Mediator
+    {
+        virtual ~Mediator() = default;
+
+        virtual void notify_read() = 0;
+        virtual void notify_write() = 0;
+
+        virtual event_base* event_base() = 0;
+
+        virtual std::string const& peer_congestion_algorithm() = 0;
+        virtual tr_address bind_address(tr_address_type type) = 0;
+        virtual tr_tos_t tos() = 0;
+    };
+
+    explicit tr_peer_transport(tr_socket_address const& socket_address)
+        : socket_address_{ socket_address }
+    {
+    }
+
+    virtual ~tr_peer_transport() = default;
+
+    [[nodiscard]] virtual bool is_valid() const noexcept
+    {
+        return socket_address_.is_valid();
+    }
+
+    [[nodiscard]] constexpr auto const& socket_address() const noexcept
+    {
+        return socket_address_;
+    }
+
+    [[nodiscard]] constexpr auto const& address() const noexcept
+    {
+        return socket_address_.address();
+    }
+
+    [[nodiscard]] constexpr auto port() const noexcept
+    {
+        return socket_address_.port();
+    }
+
+    [[nodiscard]] auto display_name() const
+    {
+        return socket_address_.display_name();
+    }
+
+    virtual size_t recv(InBuf& buf, size_t n_bytes, tr_error* error = nullptr) = 0;
+    virtual size_t send(OutBuf& buf, size_t n_bytes, tr_error* error = nullptr) = 0;
+
+    virtual void set_enabled(tr_direction dir, bool is_enabled) = 0;
+
+protected:
+    tr_socket_address socket_address_;
+};
+
+struct tr_peer_tcp : public tr_peer_transport
+{
+    explicit tr_peer_tcp(tr_socket_address const& socket_address)
+        : tr_peer_transport{ socket_address }
+    {
+    }
+
+    static std::unique_ptr<tr_peer_tcp> create(
+        tr_socket_address const& socket_address,
+        bool client_is_seed,
+        std::unique_ptr<Mediator> mediator);
+};
+
+struct tr_peer_utp : public tr_peer_transport
+{
+    explicit tr_peer_utp(tr_socket_address const& socket_address)
+        : tr_peer_transport{ socket_address }
+    {
+    }
+
+    static std::unique_ptr<tr_peer_utp> create(tr_socket_address const& socket_address, std::unique_ptr<Mediator> mediator);
+};
