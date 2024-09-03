b9047315bb282a41f3d2d73a69372e7410fdecb6
Yat Ho <lagoho7@gmail.com>
2024-09-03 16:39:31 +0800

    refactor: change `session-stats` to `session_stats`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2470,7 +2470,7 @@ auto constexpr SyncHandlers = std::array<std::pair<std::string_view, SyncHandler
     { "session-close"sv, sessionClose },
     { "session_get"sv, sessionGet },
     { "session_set"sv, sessionSet },
-    { "session-stats"sv, sessionStats },
+    { "session_stats"sv, sessionStats },
     { "torrent_get"sv, torrentGet },
     { "torrent_reannounce"sv, torrentReannounce },
     { "torrent_remove"sv, torrentRemove },
