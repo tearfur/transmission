231ed8f644372848700cbf9b3e02bb8af760ad42
Yat Ho <lagoho7@gmail.com>
2026-01-28 21:47:29 +0800

    chore(gtk): silence warnings

--- libtransmission/variant.h
+++ libtransmission/variant.h
@@ -91,7 +91,7 @@ public:
             {
                 return item.first == key;
             };
-            return std::find_if(std::begin(vec_), std::end(vec_), predicate);
+            return std::ranges::find_if(vec_, predicate);
         }
 
         [[nodiscard]] constexpr auto find(tr_quark const key) const noexcept
@@ -101,6 +101,7 @@ public:
 
         [[nodiscard]] constexpr auto contains(tr_quark const key) const noexcept
         {
+            // NOLINTNEXTLINE(readability-container-contains)
             return find(key) != end();
         }
 
